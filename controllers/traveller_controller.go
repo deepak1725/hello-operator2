324a56116aa561bf44b34402287edbfa87131cc6
Deepak Sharma <deepshar@redhat.com>
2021-08-14 15:54:33 +0530

    controller logic

--- controllers/traveller_controller.go
+++ controllers/traveller_controller.go
@@ -19,10 +19,14 @@ package controllers
 import (
 	"context"
 
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	mydomainv1alpha1 "hello-operator2/api/v1alpha1"
 )
@@ -47,9 +51,42 @@ type TravellerReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
 func (r *TravellerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx).WithValues("Traveller", req.NamespacedName)
 
-	// your logic here
+	// Fetch the Traveller instance
+	instance := &mydomainv1alpha1.Traveller{}
+	err := r.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	// Check if this Deployment already exists
+	found := &appsv1.Deployment{}
+	err = r.Get(context.TODO(), types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, found)
+	var result *reconcile.Result
+	result, err = r.ensureDeployment(req, instance, r.backendDeployment(instance))
+	if result != nil {
+		log.Error(err, "Deployment Not ready")
+		return *result, err
+	}
+
+	// Check if this Service already exists
+	result, err = r.ensureService(req, instance, r.backendService(instance))
+	if result != nil {
+		log.Error(err, "Service Not ready")
+		return *result, err
+	}
+
+	// Deployment and Service already exists - don't requeue
+	log.Info("Skip reconcile: Deployment and service already exists",
+		"Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
 
 	return ctrl.Result{}, nil
 }
